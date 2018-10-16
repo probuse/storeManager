b62151d2ded52d2e18575fce772dcc2310412b72
probuse <nprobuse@gmail.com>
2018-10-16 07:37:04 +0300

    [delivers #161076879] add pop up to show that login was successful

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-function getFormData() {
-    const emailAddress = document.getElementById("emailAddress").value;
-
-    if (emailAddress.startsWith("admin")) {
-        alert("This is an admin")
-        window.location.ref = "./store_owner_sales.html";
-    }
+function getLoginFormData() {
+    alert("Login Successful");
 }
